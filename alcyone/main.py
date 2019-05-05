cce6c259c35e51fa964c9af72bfeacb126b35bdf
glucksfall <glucksfall@users.noreply.github.com>
2019-05-05 19:26:18 -0400

    read reports

--- alcyone/main.py
+++ alcyone/main.py
@@ -345,10 +345,10 @@ def read_reports():
 	for folder in reports:
 		last_outmodels = sorted(glob.glob(folder + '/{ranking}/*'.format(**opts)))[-1]
 		with open(last_outmodels, 'r') as infile:
-			tmp.append(pandas.read_csv(infile, delimiter = '\t', skiprows = 4, header = 0, engine = 'python').iloc[0,:].T)
+			tmp.append(pandas.read_csv(infile, delimiter = '\t', skiprows = 4, header = 0, engine = 'python').iloc[0, :].T)
 
 	tmp = pandas.concat(tmp, keys = range(len(tmp)))
-	print(tmp)
+	print(tmp.T)
 
 	return 0
 
