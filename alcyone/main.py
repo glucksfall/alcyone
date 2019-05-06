44ceebcf69c0c2d454821921f4f5bce35d304cfb
glucksfall <glucksfall@users.noreply.github.com>
2019-05-05 20:09:18 -0400

    read reports

--- alcyone/main.py
+++ alcyone/main.py
@@ -96,7 +96,7 @@ def argsparser():
 	parser.add_argument('--nobs'   , metavar = 'int'  , type = int  , required = True , nargs = 1  , help = 'number of replications to draw from original data')
 	parser.add_argument('--soft'   , metavar = 'str'  , type = str  , required = True , nargs = 1  , help = 'one of the compatible stochastic software: bng2, kasim4, nfsim, piskas')
 	# not required arguments for alcyone
-	parser.add_argument('--alpha'  , metavar = 'float', type = str  , required = False, default = '.95', nargs = 1, help = 'maximum confidence interval (depends on number of runs)')
+	parser.add_argument('--alpha'  , metavar = 'float', type = float, required = False, default = 0.95, nargs = 1,  help = 'maximum confidence interval (depends on number of runs)')
 
 	# required arguments for pleione
 	parser.add_argument('--model'  , metavar = 'str'  , type = str  , required = True  , nargs = 1  , help = 'RBM with tagged variables to parameterize')
